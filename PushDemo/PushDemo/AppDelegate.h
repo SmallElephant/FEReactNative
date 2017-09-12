336ded051669591175b5a9ad432feb13a568c8ed
xiangdefei <xiangdefei@we.com>
2017-09-12 15:14:00 +0800

    初始化

---
+++ PushDemo/PushDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PushDemo
+//
+//  Created by FlyElephant on 2017/9/12.
+//  Copyright © 2017年 FlyElephant. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
