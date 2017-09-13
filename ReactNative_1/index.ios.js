3e4c562b6fa681728b8dad0cbcfd34551185dc35
xiangdefei <xiangdefei@we.com>
2017-09-13 19:14:17 +0800

    返回箭头模式修改

--- ReactNative_1/index.ios.js
+++ ReactNative_1/index.ios.js
@@ -5,6 +5,7 @@ import {
   AppRegistry,
   StyleSheet,
   Text,
+  Button,
   View
 } from 'react-native';
 
@@ -38,17 +39,54 @@ import { StackNavigator } from 'react-navigation';
 //   },
 // });
 
+class ChatScreen extends React.Component {
+
+    // const {goBack} = this.props.navigation;
+    // Nav options can be defined as a function of the screen's props:
+
+
+    // Nav options can be defined as a function of the screen's props:
+    static navigationOptions = ({ navigation }) => ({
+        title: `Chat with ${navigation.state.params.user}`,
+        headerRight: <Button title="Info1" />,
+        headerRight: (<TouchableHighlight
+            onPress={() => navigation.navigate('Home')}
+            underlayColor={'#444444'}
+        >
+            <Image source={ require('./app/left-arrow.png')} />
+        </TouchableHighlight>)
+    });
+
+    render() {
+        return (
+            <View>
+                <Text>Chat with Lucy</Text>
+            </View>
+        );
+    }
+}
+
 class HomeScreen extends React.Component {
     static navigationOptions = {
         title: 'Welcome',
     };
     render() {
-        return <Text>Hello, Navigation!</Text>;
+        const { navigate } = this.props.navigation;
+        return (
+            <View>
+                <Text>Hello, Chat App!</Text>
+                <Button
+                    onPress={() => navigate('Chat', {user: 'FlyElephant'}) }
+                    title="Chat with Lucy"
+                />
+            </View>
+        );
     }
 }
 
 const rootApp = StackNavigator({
     Home: { screen: HomeScreen },
+    Chat: { screen: ChatScreen }
 });
 
 // 整体js模块的名称
